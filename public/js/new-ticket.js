a35d8dec2ca3820e9f031c23ded352ac639bd699
Duvan Smith <dsmith.mr@gmail.com>
2024-12-28 16:35:00 -0500

    Logica new tickets

--- public/js/new-ticket.js
+++ public/js/new-ticket.js
@@ -1,1 +1,23 @@
-console.log('Nuevo Ticket HTML');
+const currentTicketLbl = document.querySelector("span");
+const createTicketBtn = document.querySelector("button");
+
+async function getLasTicket() {
+  const response = await fetch("/api/tickets/last");
+  const data = await response.json();
+  currentTicketLbl.innerText = data;
+}
+
+async function createTicket() {
+  const response = await fetch("/api/tickets", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const data = await response.json();
+  currentTicketLbl.innerText = data.number;
+}
+
+createTicketBtn.addEventListener("click", createTicket);
+
+getLasTicket();
