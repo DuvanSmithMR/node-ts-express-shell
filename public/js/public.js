6ed433b52673c27e809683fe9bc7715e70cc34c9
Duvan Smith <dsmith.mr@gmail.com>
2024-12-29 10:43:56 -0500

    Logica de mostrado de tickets

---
+++ public/js/public.js
@@ -0,0 +1,22 @@
+function renderTickets(tickets=[]) {
+    for(let i=0;i<tickets.length;i++){
+        if(i>=4) break;
+        
+        const ticket = tickets[i];
+
+        if(!ticket) continue;
+
+        const lblTicket= document.querySelector(`#lbl-ticket-0${i+1}`);
+        const lblDesk= document.querySelector(`#lbl-desk-0${i+1}`);
+
+        lblTicket.innerText = `Ticket ${ticket.number}`;
+        lblDesk.innerText = ticket.handleAtDesk;
+    }
+}
+
+async function loadCurrentTickets() {
+    const tickets = await fetch('/api/tickets/working-on').then(res => res.json());
+    renderTickets(tickets);
+}
+
+loadCurrentTickets();
